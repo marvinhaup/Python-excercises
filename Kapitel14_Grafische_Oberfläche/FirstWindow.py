cc3ccd00b34aa70f4b25fe2e7fd12f7e5634a14d
Marvin Haupenthal <76146648+marvinhaup@users.noreply.github.com>
2021-05-14 10:33:58 +0200

    modified first tkinter test, added second one

---
+++ Kapitel14_Grafische_Oberfläche/FirstWindow.py
@@ -0,0 +1,41 @@
+# Programm zum Testen der tkinter-Bibliothek
+
+from tkinter import *
+
+# defining TK-window
+window = Tk()
+
+# Tkinter- window sizes
+windowWidth = 300
+windowHeight = 100
+
+
+# creating an underclass, to define what to do on button-pressing
+class MyButton(Button):
+    def action(self):
+        print("Sie haben den Knopf gedrückt!")
+
+# setting up the window reaolution
+screenResolution = (
+    str(windowWidth) + "x" + str(windowHeight)
+)  # workaround to avoid hard-coding tkinter windows
+window.geometry(screenResolution)
+
+# defining and adding a border to the window
+border = Frame(window, relief="ridge", borderwidth=5)
+border.pack(fill="both", expand=1)
+
+# adding strings into the tkinter output
+window.title("Python-Kurs")
+label = Label(border, text="Willkommen zum Python-Kurs!")
+label.pack(expand=1)
+
+# defining a tkinter-button
+button = Button(border, text="OK", command=window.destroy)
+button.pack(side="bottom")
+
+button2 = MyButton(border, text="Action!")
+button2["command"] = button2.action
+button2.pack(side = "bottom")
+
+window.mainloop()
